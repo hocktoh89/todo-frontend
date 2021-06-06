b1b07bc6dad6f2a27408995fc6277e3ffe107cf5
Thomas Yap Hock Toh <yhtthomas@gmail.com>
2021-06-06 21:15:59 +0800

    test: enhance editToDo test with mocking backend api

--- cypress/integration/editToDo.spec.js
+++ cypress/integration/editToDo.spec.js
@@ -1,6 +1,7 @@
 const todoItems = 
 [
-    "Buy Apples and Bananas",
+    "watch Movie",
+    "Play Piano",
     "Buy Chicken Rice"
 ];
 
@@ -9,34 +10,31 @@ const editedToDo = "See Doctor"
 const expectedResults = 
 [
     todoItems[0],
-    editedToDo
+    editedToDo,
+    todoItems[2],
 ];
 
 before(() => {
+    cy.intercept('GET', '/todos', { fixture: 'todos.json' });
+    cy.intercept('PUT', '/todos/*', {
+        statusCode: 200,
+        body: {
+            data: {
+                _id: "60bcb7549714ae510dd8316b",
+                text: editedToDo,
+                __v: 0
+            }
+        },
+    });
+
     cy.visit("http://localhost:3000");
-})
-  
-after(() => {
-    cy.get('[data-cy=deleteToDosBtn]').click();
-})
-
-beforeEach(() => {
-    cy.get('[data-cy=newToDoBtn]').within(() => {
-        cy.get('input').type(todoItems[0])
-        cy.get('button').contains('Add').click()
-      })
-
-      cy.get('[data-cy=newToDoBtn]').within(() => {
-        cy.get('input').type(todoItems[1])
-        cy.get('button').contains('Add').click()
-      })
 })
 
 describe('Edit To Do Item', () => {
 
 
     it('Edit successful', () => {
-        cy.get('[data-cy=toDoList]').find('li').should('have.length', 2);
+        cy.get('[data-cy=toDoList]').find('li').should('have.length', 3);
         cy.get('[data-cy=toDoList]').find('input').eq(0).should('have.value', todoItems[0]);
 
         cy.get('[data-cy=toDoList]').find('li').eq(1).find('[data-cy=editToDoBtn]').click();
